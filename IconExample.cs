b697f956a0f20d18a8361d63e8f7fc7dddee958c
ballio001 <onur.b@hotmail.nl>
2024-01-05 15:22:07 +0100

    init commit 2

--- IconExample.cs
+++ IconExample.cs
@@ -6,8 +6,8 @@ public partial class IconExample : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		int i = 45;
-		string test = "test";
+		int i = 4544;
+		string test = "test2";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
